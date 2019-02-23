1ab475a2154420f8776911e6cbf78f4f9e3f4450
John Pena <jpena91@gmail.com>
2019-02-23 16:30:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+ my_hash={BMW => "5 Series", Jeep => "Laredo", Mercedes => "AMG"} 
 end
 
 
